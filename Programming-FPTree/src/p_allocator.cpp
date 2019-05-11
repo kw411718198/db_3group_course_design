ae356c1aa673f1f5db13ccddbf1f81dda636f4ac
kongwei <49843143+kw411718198@users.noreply.github.com>
2019-05-11 23:31:09 +0800

    Update p_allocator.cpp

--- Programming-FPTree/src/p_allocator.cpp
+++ Programming-FPTree/src/p_allocator.cpp
@@ -1,13 +1,12 @@
-#include<string>
 #include"utility/p_allocator.h"
 #define LAYOUT_NAME "my_layout"
 #include<iostream>
 using namespace std;
 
 // the file that store the information of allocator
-const string P_ALLOCATOR_CATALOG_NAME = "p_allocator_catalog";//file
+const string P_ALLOCATOR_CATALOG_NAME = "p_allocator_catalog";
 // a list storing the free leaves
-const string P_ALLOCATOR_FREE_LIST    = "free_list";//file
+const string P_ALLOCATOR_FREE_LIST    = "free_list";
 
 PAllocator* PAllocator::pAllocator = new PAllocator();
 
@@ -33,39 +32,44 @@ PAllocator::PAllocator() {
     if (allocatorCatalog.is_open() && freeListFile.is_open()) {
         // exist
         // TODO
-        maxFileId=0;
-        pAllocator=nullptr;
+        catalog = std::move(pmem_ptr<allocator_catalog>{catalogPath});
+
+        ifstream freelist(freeListFile, ios::in|ios::binary);
+        for (uint64_t i = 0; !freelist.eof();i ++) {
+            PPointer p;
+            freelist >> p;
+            freeList.push_back(p);
+        }
        // initFilePmemAddr();
 
     } else {
         // not exist, create catalog and free_list file, then open.
         // TODO
-        ifstream allocatorCatalog(allocatorCatalogPath, ios::in|ios::binary);
-        ifstream freeListFile(freeListPath, ios::in|ios::binary);
-         maxFileId=0;
-        pAllocator=nullptr;
+        ofstream allocatorCatalog(allocatorCatalogPath, ios:out|ios::binary);
+        ofstream freeListFile(freeListPath, ios::out|ios::binary);
     }
     this->initFilePmemAddr();
 }
-// vector<PPointer>     freeList;
+// vector<PPointer>     freeList;  
 PAllocator::~PAllocator() {
     // TODO
-    freeList.clear();
     fId2PmAddr.clear();
+    PAllocator::pAllocator = nullptr;
 }
 
 // memory map all leaves to pmem address, storing them in the fId2PmAddr
 void PAllocator::initFilePmemAddr() {
     // TODO
-    PPointer p = startLeaf;
     char *pmemaddr;
     size_t mapped_len;
     int is_pmem;
-
-    for(size_t i=0;i<freeList.size();i++){
+   string freeListPath         = DATA_DIR + P_ALLOCATOR_FREE_LIST;
+    bool flag = freeListPath.is_open();
+    fId2PmAddr.clear();
+    for(unint64_t i=0;i<freeList.size();i++){
         //cout<<freeList[i].fileId;
-        char * addr = pmem_map_file(const char *path, size_t len, int flags, mode_t mode, size_t *mapped_lenp, int *is_pmemp);
-        fId2PmAddr.insert(map<uint64_t, char*>::value_type(freeList[i].fileId,addr));
+        char * addr = pmem_map_file(freeListPath, freeNum, flag, 0666, &mapped_len, &is_pmem);
+        fId2PmAddr.insert(map<uint64_t, char*>::value_type(freeList[i].fileId),addr);
     }
 
 }
@@ -73,60 +77,64 @@ void PAllocator::initFilePmemAddr() {
 // get the pmem address of the target PPointer from the map fId2PmAddr
 char* PAllocator::getLeafPmemAddr(PPointer p) {
     // TODO
-    return fId2PmAddr.find(p.fileId)->second;
+    return fId2PmAddr.find(p.fileId)->second+p.offset;
 //    return NULL;
 }
 
 // get and use a leaf
 //the fptree leaf allocation
-// return
+// return 
 bool PAllocator::getLeaf(PPointer &p, char* &pmem_addr) {
-    // TODO
-    freeList.push_back(p);
-    fId2PmAddr.insert(map<uint64_t, char*>::value_type(p.fileId,pmem_addr));
-    freeNum--;
-    return false;
+   if(getFreeNum() == 0){
+        if(!newLeafGroup())return false;
+   }
+    uint64_t n = fId2PmAddr[p.fileId++];
+    pmem_addr = n + p.offset;
+    return true;
 }
 
-bool     PAllocator::ifLeafUsed(PPointer p){
-    //rely on whether the bitmap
-    return true;
+bool PAllocator::ifLeafUsed(PPointer p){
+    bitmap[(p.offset - LEAF_GROUP_AMOUNT) / sizeof(leaf)];
+    return ifLeafExist(p) && ((p.offset - LEAF_GROUP_AMOUNT) / sizeof(leaf) < usedNum?true:false);
 
 }
 bool PAllocator::ifLeafFree(PPointer p) {
     // TODO
-    //vector<PPointer>     freeList;
-    for(auto it=freeList.begin();it!=freeList.end();it++){
-        if(*it== p)
-            return true;
-    }
-    return false;
+    return !ifLeafUsed(p); 
 }
 
-// judge whether the leaf with specific PPointer exists.
+// judge whether the leaf with specific PPointer exists. 
 bool PAllocator::ifLeafExist(PPointer p) {//if p.fid is in the catalog
     // TODO
-    if(ifLeafFree(p) || ifLeafUsed(p))
-    return true;
+    if(fId2PmAddr.count(p.fileId) && fId2PmAddr.count(p.fileId) != '\0'){
+        return true;
+    }
+    return false;
+    
 }
 
 // free and reuse a leaf
 bool PAllocator::freeLeaf(PPointer p) {
     // TODO
-    PPointer p;
-    freeList.insert(freeList.end(),p);
-    freeNum++;
+    if(ifLeafExist(p)){
+        memset(fId2PmAddr[p.fileId],'\0',sizeof(p.offset))
+         freeNum ++;
+        return true;
+    }
     return false;
 }
 // persist the catalog file in NVM/SSD
 bool PAllocator::persistCatalog() {
     // TODO
-   // const void *addr, size_t len;
-    for(auto it=freeList.begin();it!=freeList.end();it++){
-       // if(*it== p)
-        pmem_persist(getLeafPmemAddr(*it), it->offset);
+    string freeListPath         = DATA_DIR + P_ALLOCATOR_FREE_LIST;
+    ifstream freelist(freeListPath));
+    if(freelist.is_open()){
+        return false;
     }
-    return false;
+    for(unit64_t i = 0;i < freeNum;i ++){
+        freelist <<freeList[i];
+    }
+    return true;
 }
 
 /*
@@ -136,19 +144,16 @@ bool PAllocator::persistCatalog() {
 // create a new leafgroup, one file per leafgroup
 bool PAllocator::newLeafGroup() {
     // TODO
-    //
-    //string s = maxFileId;
-    maxFileId++;
-    uint64_t max = maxFileId;
-    string fileName = std::to_string(max);
-    //name use the integer from 1 ,every time plus 1
+    persistent_ptr<leaf_group> group(getMaxFileId(),0)
+    if(!group)
+        return false;
+    for(size_t i = 0;i < LEAF_GROUP_AMOUNT;i++){
+        freeList.push_back(new PPoint().offset);
+    }
 
-    string allocatorLeafGroupPath = DATA_DIR + fileName;
-   // string freeListPath         = DATA_DIR + P_ALLOCATOR_FREE_LIST;
-    ifstream allocatorLeafGroup(allocatorLeafGroupPath, ios::in|ios::binary);
-    //ifstream freeListFile(freeListPath, ios::in|ios::binary);
-    // judge if the catalog exists
-    return false;
+    fId2PmAddr.insert(getMaxFileId(), group);
+    maxFileId++;
+    return true;
 }
 //    PPointer getUsedLeaf(int idx);
 //    PPointer getFreeLeaf(int idx);
